22161a04b546ece9f79730ad716fa075c9e9395d
koyif <koyif@users.noreply.github.com>
2020-01-12 02:30:34 +0500

    Initialized the pure Spring Boot application

---
+++ src/main/java/org/koyif/singlespringbootappdockerimagesizeoptimization/SingleSpringBootAppDockerImageSizeOptimizationApplication.java
@@ -0,0 +1,13 @@
+package org.koyif.singlespringbootappdockerimagesizeoptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSpringBootAppDockerImageSizeOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSpringBootAppDockerImageSizeOptimizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/koyif/singlespringbootappdockerimagesizeoptimization/SingleSpringBootAppDockerImageSizeOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koyif.singlespringbootappdockerimagesizeoptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleSpringBootAppDockerImageSizeOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
